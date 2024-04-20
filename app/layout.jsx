5e942bc6721752ae14816e5d3380342eb7ab936e
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2024-04-20 11:40:47 +0800

    add bg

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,7 +11,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="h-full">
+    <html lang="en" className="h-full bg-white">
       <body className={clsx(inter.className, "h-full")}>{children}</body>
     </html>
   );
